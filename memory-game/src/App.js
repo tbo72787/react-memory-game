cd4b0e8d3ad77d25837b07fb25fdffcdb6d1d35c
Taylor Zettel <tzettel@gmail.com>
2020-02-26 20:46:23 -0500

    added images for game in 'images', imported them in 'app.js', and displayed them

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import PictureCard from "./components/PictureCard"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import all files from given directory
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+// use importAll to import all images and assign them to 'const images' as array
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+console.log(images);
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    images
+  };
+  render() {
+    return (
+      <div>
+        {this.state.images.map((image, i) => (
+          <PictureCard
+            id={image.id}
+            // key={image.id}
+            image={image}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
